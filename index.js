c7ec5c4b9b139383465d72d232fc27740a6982b0
barry.c.schechter@gmail.com <barry.c.schechter@gmail.com>
2018-09-03 13:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function divide (a, b) {
   return a / b
 }
 function inc(n) {
-  returns n+=
+  return n+=
 }
 function dec(n) {
-  returns n-=
+  return n-=
 }
 function makeInt(n){
   parseInt (n, 10)
